28082f207bf22c721f866cf363585d6d4df7506a
Andrey Dergach <thequasar7@gmail.com>
2021-10-16 19:59:20 +0300

    Implemented obtaining package list

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import subprocess
+
+
+def get_package_list():
+    res = subprocess.run(['pacman', '-Q'], stdout=subprocess.PIPE)
+    list = res.stdout.decode("utf-8").split("\n")
+    parsed_list = []
+    for i in range(0, len(list) - 2):
+        parsed_list.append(list[i].split(" ")[0])
+    return parsed_list
+
+
 def main():
-    pass
+    print(get_package_list())
+
 
 if __name__ == "__main__":
     main()
